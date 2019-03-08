99cf9467f8d62dae9d0f8f2a3debc27c24284585
unknown <2013096825@qqq.com>
2019-03-08 11:41:21 +0800

    now can config the signs

--- tpl-with-function.js
+++ tpl-with-function.js
@@ -1,7 +1,47 @@
-function tpl(str, data) {
-  const tplStr = str.replace(/\n/g, '')
-     .replace(/{{(.+?)}}/g, (match, p) => `' + ${p} + '`)
-     .replace(/{@(.+?)@}/g, (match, p) => ` '; ${p} tpl += '`);
-  print(tplStr)
-  return new Function('data', `let tpl=' ${tplStr}'; return tpl;`)(data);
+// function tpl(str, data) {
+//   const tplStr = str.replace(/\n/g, '')
+//      .replace(/{{(.+?)}}/g, (match, p) => `' + ${p} + '`)
+//      .replace(/{@(.+?)@}/g, (match, p) => ` '; ${p} tpl += '`);
+//   print(tplStr)
+//   return new Function('data', `let tpl=' ${tplStr}'; return tpl;`)(data);
+// }
+
+
+class Tpl {
+  constructor(config) {
+    const defaultConfig = {
+      signs: {
+        varSign: ['{{', '}}'],
+        evalSign: ['{@', '@}'],
+        commentSign: ['<!--', '-->'],
+        noCommentSign: ['{#', '#}']
+      }
+    }
+    this.config = Object.assign({}, defaultConfig, config);
+    Object.keys(this.config.signs).forEach(key => {
+      this.config.signs[key].splice(1, 0, '(.+?)');
+      this.config.signs[key] = new RegExp(this.config.signs[key].join(''), 'g');
+    })
+  }
+
+  _compile(str, data) {
+    console.log9
+    const tplStr = str.replace(/\n/g, '')
+    //注释
+     .replace(this.config.signs.noCommentSign, '')
+     .replace(this.config.signs.commentSign, (match, p) => `'+'<!--${p}-->'+'`)
+     .replace(this.config.signs.varSign, (match, p) => `' + ${p} + '`)
+     .replace(this.config.signs.evalSign, (match, p) => {
+       return ` '; ${p} tpl += '`
+      });
+      console.log(tplStr)
+    return new Function('data', `let tpl=' ${tplStr}'; return tpl;`)(data);
+  }
+  compile(str, data) {
+    return this._compile(str, data);
+  }
+}
+
+function tpl(config) {
+  return new Tpl(config);
 }
